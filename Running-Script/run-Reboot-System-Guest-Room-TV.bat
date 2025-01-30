ccb34761d40febf93b09a6d121baaa36472b3283
MeSkipperr <ikadekyolaandika02@gmail.com>
2025-01-30 23:57:03 +0800

    add pause to make cmd not auto close when script finnis running

--- Running-Script/run-Reboot-System-Guest-Room-TV.bat
+++ Running-Script/run-Reboot-System-Guest-Room-TV.bat
@@ -2,3 +2,6 @@
 cd /d "%~dp0"
 cd ..
 node .\Project\Reboot-System-Guest-Room-TV\
+
+echo Script execution completed. Press any key to exit...
+pause
--- Running-Script/run-enable-youtube.bat
+++ Running-Script/run-enable-youtube.bat
@@ -2,3 +2,6 @@
 cd /d "%~dp0"
 cd ..
 node .\Project\Enable-Youtube\
+
+echo Script execution completed. Press any key to exit...
+pause
